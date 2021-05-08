b7fa93f4a603fa5984ff457369885b8def209135
Laura Heimann <hello@laura.media>
2021-05-08 23:15:13 +0200

    added functions for gameScreenshot management

---
+++ src/gameScreenshots.js
@@ -0,0 +1,96 @@
+const bcrypt = require('bcrypt');
+const chalk = require('chalk');
+const fs = require("fs");
+const readChunk = require('read-chunk');
+const imageType = require('image-type');
+const uniqid = require('uniqid');
+const path = require("path");
+const { Game, GameScreenshot } = require('../sequelize');
+
+function getGameScreenshots( searchOptions ) {
+    return new Promise((resolve, reject) => {
+        GameScreenshot.findAll({ where: searchOptions }).then((gameScreenshotData) => {
+            resolve(gameScreenshotData);
+        });
+    });
+}
+function getOneGameScreenshot( searchOptions ) {
+    return new Promise((resolve, reject) => {
+        GameScreenshot.findOne({ where: searchOptions }).then((gameScreenshotData) => {
+            resolve(gameScreenshotData);
+        }).catch((error) => {
+            console.error(error);
+            reject(error);
+        });
+    });
+}
+
+function saveGameScreenshot( game, files ) {
+    return new Promise((resolve, reject) => {
+        files.forEach((screenshotFile) => {
+            // Filetype Check
+            let screenshotImageBuffer = readChunk.sync(screenshotFile.path, 0, 12);
+            let screenshotImageType = imageType(screenshotImageBuffer);
+    
+            // Ignore non-images
+            if(screenshotImageType === null) {
+                return;
+            }
+    
+            // Only allow PNG & JPG images
+            if(screenshotImageType.mime !== "image/jpeg" && screenshotImageType.mime !== "image/png") {
+                return;
+            }
+    
+            let newImageName = uniqid() + "." + screenshotImageType.ext;
+            let newImagePath = path.join("./public/gameScreenshots/" + newImageName);
+            fs.renameSync(screenshotFile.path, newImagePath);
+    
+            let data = {
+                gameId: game.id,
+                fileName: newImageName
+            };
+    
+            // TODO: Minimize images
+    
+            // Save database Entries
+            GameScreenshot.create(data).catch((error) => {
+                fs.unlinkSync(newImagePath);
+                reject(error);
+            });
+        });
+        
+        resolve();
+    });
+}
+
+function deleteGameScreenshot( gameScreenshotID ) {
+    return new Promise((resolve, reject) => {
+        getOneGameScreenshot( { id: gameScreenshotID }).then((data) => {
+            if(data === null) {
+                reject(404);
+                return;
+            }
+
+            let imagePath = path.join("./public/gameScreenshots/" + data.fileName);
+            
+            // remove physical file
+            fs.unlinkSync(imagePath);
+
+            GameScreenshot.destroy( { where: { id: gameScreenshotID } } ).then(() => {
+                resolve();
+            }).catch((error) => {
+                reject(error);
+            });
+        }).catch((error) => {
+            reject(error);
+        });
+    });
+}
+
+module.exports = {
+    getGameScreenshots,
+    getOneGameScreenshot,
+    saveGameScreenshot,
+    deleteGameScreenshot
+}
