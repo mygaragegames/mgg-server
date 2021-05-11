1d59e9d1152535d85908e7831c484348c74541a0
Laura Heimann <hello@laura.media>
2021-05-11 17:17:13 +0200

    added addGameToPlaylist and removeGameToPlaylist routes

--- routes/v1/playlists.js
+++ routes/v1/playlists.js
@@ -13,6 +13,12 @@ router.route('/:playlistid')
     .put(auth.verifyToken, putOneHandler)
     .delete(auth.verifyToken, deleteOneHandler);
 
+router.route('/:playlistid/add/:gameid')
+    .post(auth.verifyToken, postAddHandler);
+
+router.route('/:playlistid/delete/:gameid')
+    .delete(auth.verifyToken, deleteRemoveHandler);
+
 async function getOneHandler(req, res) {
     console.log(chalk.grey("[mgg-server] (Playlists) Playlists->Get"));
 
@@ -100,6 +106,56 @@ async function deleteOneHandler(req, res) {
         res.status(200).json({name: "PLAYLIST_DELETED", text: "Playlist was deleted."});
         return;
     }).catch((error) => {
+        res.status(500).json({name: "UNKNOWN_SERVER_ERROR", text: "Unknown Server Error! Please try again later!"});
+    });
+}
+
+async function postAddHandler(req, res) {
+    console.log(chalk.grey("[mgg-server] (Playlists) Playlists->AddGame"));
+
+    let playlist = await getOnePlaylist({ id: parseInt(req.params.playlistid) }).catch(() => { return null; });
+    if(playlist === null) {
+        res.status(404).json({name: "PLAYLIST_NOT_FOUND", text: "There is no playlist with the id " + req.params.playlistid});
+        return;
+    }
+
+    // Check if user is owner or moderator/admin
+    if(playlist.userId !== req.userId && !req.userRoles.includes('moderator', 'admin')) {
+        res.status(403).json({name: "AUTHENTICATION_NEEDED", text: "You are not allowed to perform this action."});
+        return;
+    }
+
+    playlist.addGame(req.params.gameid).then((data) => {
+        res.status(201).json({name: "PLAYLIST_GAME_ADDED", text: "Game was added to playlist."});
+    }).catch((error) => {
+        if(error.name === 'SequelizeUniqueConstraintError') {
+            res.status(409).json({name: "PLAYLIST_GAME_CONFLICT", text: "Game is already in playlist."});
+        } else {
+            console.error(error);
+            res.status(500).json({name: "UNKNOWN_SERVER_ERROR", text: "Unknown Server Error! Please try again later!"});
+        }
+    });
+}
+
+async function deleteRemoveHandler(req, res) {
+    console.log(chalk.grey("[mgg-server] (Playlists) Playlists->RemoveGame"));
+
+    let playlist = await getOnePlaylist({ id: parseInt(req.params.playlistid) }).catch(() => { return null; });
+    if(playlist === null) {
+        res.status(404).json({name: "PLAYLIST_NOT_FOUND", text: "There is no playlist with the id " + req.params.playlistid});
+        return;
+    }
+
+    // Check if user is owner or moderator/admin
+    if(playlist.userId !== req.userId && !req.userRoles.includes('moderator', 'admin')) {
+        res.status(403).json({name: "AUTHENTICATION_NEEDED", text: "You are not allowed to perform this action."});
+        return;
+    }
+
+    playlist.removeGame(req.params.gameid).then((data) => {
+        res.status(200).json({name: "PLAYLIST_GAME_REMOVED", text: "Game was deleted from playlist."});
+    }).catch((error) => {
+        console.error(error);
         res.status(500).json({name: "UNKNOWN_SERVER_ERROR", text: "Unknown Server Error! Please try again later!"});
     });
 }
