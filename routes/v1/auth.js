b46d9731f6232c8992badb9cc02530accc955f62
Laura Heimann <hello@laura.media>
2021-06-01 20:36:00 +0200

    added apidocs to auth routes

--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -6,6 +6,8 @@ const { parseAvatar, isEmailValid } = require('../../src/parsers');
 const { login, verify, update } = require('../../src/auth');
 const { getOneUser } = require('../../src/users');
 
+let isDev = process.env.NODE_ENV !== 'prod';
+
 router.route('/login')
     .post(postLoginHandler);
 
@@ -15,8 +17,23 @@ router.route('/verify')
 router.route('/update/:userid')
     .put(auth.verifyToken, putUpdateHandler);
 
+/**
+ * @api {post} /auth/login Login a User
+ * @apiName AuthLogin
+ * @apiGroup Auth
+ * 
+ * @apiParam {String} username Username
+ * @apiParam {String} password Password
+ * 
+ * @apiSuccess (200) {Object} userData Object of the authenticated User
+ * @apiSuccess (200) {String} token JWT Token for authenticated calls
+ * @apiSuccess (200) {Array} roles Roles of the authenticated User
+ * @apiError (404) USER_NOT_FOUND There is no user with the username <code>username</code>
+ * @apiError (403) AUTHENTICATION_BANNED Your account was banned. (Reason included in body)
+ * @apiError (403) AUTHENTICATION_WRONG Password is not correct
+ */
 async function postLoginHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Auth) Auth->Login"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Auth) Auth->Login"));
 
     if(req.body.username === '' || req.body.password === '') {
         res.status(400).json({name: "MISSING_FIELDS", text: "Required fields: username, password"});
@@ -49,8 +66,21 @@ async function postLoginHandler(req, res) {
     });
 }
 
+/**
+ * @api {post} /auth/verify Verifies a JWT token
+ * @apiName AuthVerify
+ * @apiGroup Auth
+ * 
+ * @apiParam {String} token JWT Token
+ * 
+ * @apiSuccess (200) {Object} userData Object of the authenticated User
+ * @apiSuccess (200) {String} token JWT Token for authenticated calls
+ * @apiSuccess (200) {Array} roles Roles of the authenticated User
+ * @apiError (403) AUTHENTICATION_BANNED Your account was banned. (Reason included in body)
+ * @apiError (403) AUTHENTICATION_WRONG Token is invalid
+ */
 async function postVerifyHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Auth) Auth->Verify"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Auth) Auth->Verify"));
 
     if(req.body.token === '') {
         res.status(400).json({name: "MISSING_FIELDS", text: "Required fields: token"});
@@ -80,8 +110,26 @@ async function postVerifyHandler(req, res) {
     });
 }
 
+/**
+ * @api {put} /auth/update/:userid Updates a Users email/password
+ * @apiName AuthUpdate
+ * @apiGroup Auth
+ * 
+ * @apiHeader {String} x-access-token JWT Token for authentication
+ * @apiParam {String} userid The ID of the User
+ * @apiParam {String} email [Optional] New email for User
+ * @apiParam {String} password [Optional] New password for User
+ * 
+ * @apiSuccess (201) USER_UPDATED User was updated.
+ * @apiError (404) USER_NOT_FOUND There is no user with the id <code>id</code>
+ * @apiError (406) EMAIL_INVALID Email is not valid!
+ * @apiError (309) USERNAME_EMAIL_CONFLICT Username or email is already in use!
+ * @apiError (403) AUTHENTICATION_BANNED Your account was banned. (Reason included in body)
+ * @apiError (403) AUTHENTICATION_WRONG You are not allowed to perform this action.
+ * @apiError (403) AUTHENTICATION_NEEDED You are not allowed to perform this action.
+ */
 async function putUpdateHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Auth) Auth->Update"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Auth) Auth->Update"));
 
     if(req.params.userid === '') {
         res.status(400).json({name: "MISSING_FIELDS", text: "Required fields: userId"});
