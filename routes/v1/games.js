c525f2f21e287d64ea8400323ac170d56e1e8881
Laura Heimann <hello@laura.media>
2021-06-01 20:36:35 +0200

    added apidocs to games routes

--- routes/v1/games.js
+++ routes/v1/games.js
@@ -7,6 +7,8 @@ const { parseAvatar, parseGameScreenshot, parseGameCover } = require('../../src/
 const { getAllGames, getOneGame, createGame, deleteGame, updateGame, saveGameCover, deleteGameCover } = require('../../src/games');
 const { getOnePlaylist } = require('../../src/playlists');
 
+let isDev = process.env.NODE_ENV !== 'prod';
+
 let upload = multer({ dest: '/tmp/'});
 
 router.route('/')
@@ -22,8 +24,17 @@ router.route('/:gameid/cover')
     .put(auth.verifyToken, upload.single('cover'), putOneCoverHandler)
     .delete(auth.verifyToken, deleteOneCoverHandler);
 
+/**
+ * @api {get} /games Get all Games
+ * @apiName GetAllGames
+ * @apiGroup Games
+ * 
+ * @apiHeader {String} x-access-token (Optional) JWT Token for authentication
+ * 
+ * @apiSuccess (200) {Array} games Array of Games
+ */
 async function getAllHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Games) Games->Get"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Games) Games->Get"));
 
     let gamesData = await getAllGames();
 
@@ -54,8 +65,35 @@ async function getAllHandler(req, res) {
 
     res.status(200).json(gamesData);
 }
+
+/**
+ * @api {get} /games/:gameId Get detailled information from a Game
+ * @apiName GetOneGame
+ * @apiGroup Games
+ * 
+ * @apiHeader {String} x-access-token (Optional) JWT Token for authentication
+ * @apiParam {Integer} gameId The ID of the Game
+ * 
+ * @apiSuccess (200) {Integer} game.id ID
+ * @apiSuccess (200) {String} game.title Title
+ * @apiSuccess (200) {String} game.ingameID Ingame-ID
+ * @apiSuccess (200) {String} game.description Description
+ * @apiSuccess (200) {String} game.coverFileName URL of the cover
+ * @apiSuccess (200) {String} game.youtubeID Trailer YouTube-ID of the Game
+ * @apiSuccess (200) {Integer} game.displayStatus Visibility of the Game. 0 = Public, 1 = Hidden, 2 = Private
+ * @apiSuccess (200) {DateTime} createAt DateTime of creation
+ * @apiSuccess (200) {DateTime} updatedAt DateTime of last change
+ * @apiSuccess (200) {Integer} game.userId ID of the User who created the Game
+ * @apiSuccess (200) {Integer} game.user Object of the User who created the Game
+ * @apiSuccess (200) {Array} game.screenshots Array of GameScreenshots
+ * @apiSuccess (200) {Array} game.channels Array of GameChannels the Game is part of
+ * @apiSuccess (200) {Array} game.comments Array of GameComments of the Game
+ * @apiSuccess (200) {Boolean} isInPlaylist If a JWT Token is provided, this bool will be true if the Game is in the "Play later" playlist of the User
+ * @apiError (404) GAME_NOT_FOUND There is no game with the id <code>gameId</code>
+ * @apiError (403) GAME_PRIVATE You are not allowed to see this game.
+ */
 async function getOneHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Games) Games->Get"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Games) Games->Get"));
 
     if(req.params.gameid == undefined) {
         res.status(400).json({name: "MISSING_FIELDS", text: "Required parameter: gameid"});
@@ -110,8 +148,36 @@ async function getOneHandler(req, res) {
 
     res.status(200).json({ game: gameData, isInPlaylist: isInPlaylist});
 }
+
+/**
+ * @api {post} /games Create a Game
+ * @apiName CreateGame
+ * @apiGroup Games
+ * 
+ * @apiHeader {String} x-access-token JWT Token for authentication
+ * @apiParam {String} title Title
+ * @apiParam {String} description (Optional) Description
+ * @apiParam {String} ingameID Ingame-ID (Syntax: G-000-000-000)
+ * @apiParam {Integer} displayStatus (Optional) Visibility of the Game. 0 = Public, 1 = Hidden, 2 = Private
+ * @apiParam {String} youtubeID (Optional) Trailer YouTube-ID of the Game
+ * 
+ * @apiSuccess (200) {Integer} id ID
+ * @apiSuccess (200) {String} title Title
+ * @apiSuccess (200) {String} ingameID Ingame-ID
+ * @apiSuccess (200) {String} description (Optional) Description
+ * @apiSuccess (200) {String} coverFileName URL of the cover
+ * @apiSuccess (200) {String} youtubeID (Optional) Trailer YouTube-ID of the Game
+ * @apiSuccess (200) {Integer} displayStatus Visibility of the Game. 0 = Public, 1 = Hidden, 2 = Private
+ * @apiSuccess (200) {DateTime} createAt DateTime of creation
+ * @apiSuccess (200) {DateTime} updatedAt DateTime of last change
+ * @apiSuccess (200) {Integer} userId ID of the User who created the Game
+ * @apiError (400) GAME_GAMEID_WRONGFORMAT The ingame ID has the wrong format (G-000-000-000).
+ * @apiError (403) AUTHENTICATION_BANNED Your account was banned. (Reason included in body)
+ * @apiError (403) AUTHENTICATION_WRONG You are not allowed to perform this action.
+ * @apiError (403) AUTHENTICATION_NEEDED You are not allowed to perform this action.
+ */
 async function postOneHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Games) Games->Post"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Games) Games->Post"));
 
     let filteredDisplayStatus = 0;
     switch(parseInt(req.body.displayStatus)) {
@@ -142,7 +208,9 @@ async function postOneHandler(req, res) {
     }
 
     createGame( data ).then((game) => {
-        game.setChannels(req.body.channels);
+        if(req.body.channels != "" && req.body.channels != undefined) {
+            game.setChannels(req.body.channels);
+        }
 
         game.coverFileName = parseGameCover(game.coverFileName);
 
@@ -150,7 +218,7 @@ async function postOneHandler(req, res) {
     }).catch((error) => {
         switch(error) {
             default:
-                res.status(500).json({name: "UNKNOWN_ERROR", text: "Game could not be updated."});
+                res.status(500).json({name: "UNKNOWN_ERROR", text: "Game could not be created."});
                 return;
             case 400:
                 res.status(400).json({name: "GAME_GAMEID_WRONGFORMAT", text: "The ingame ID has the wrong format (G-000-000-000)."});
@@ -158,8 +226,29 @@ async function postOneHandler(req, res) {
         }
     });
 }
+
+/**
+ * @api {put} /games/:gameId Update a Game
+ * @apiName UpdateGame
+ * @apiGroup Games
+ * 
+ * @apiHeader {String} x-access-token JWT Token for authentication
+ * @apiParam {Integer} gameId ID of the Game
+ * @apiParam {String} title (Optional) Title
+ * @apiParam {String} description (Optional) Description
+ * @apiParam {String} ingameID (Optional) Ingame-ID (Syntax: G-000-000-000)
+ * @apiParam {Integer} displayStatus (Optional) Visibility of the Game. 0 = Public, 1 = Hidden, 2 = Private
+ * @apiParam {String} youtubeID (Optional) Trailer YouTube-ID of the Game
+ * 
+ * @apiSuccess (201) GAME_UPDATED Game was updated.
+ * @apiError (400) GAME_GAMEID_WRONGFORMAT The ingame ID has the wrong format (G-000-000-000).
+ * @apiError (403) AUTHENTICATION_BANNED Your account was banned. (Reason included in body)
+ * @apiError (403) AUTHENTICATION_WRONG You are not allowed to perform this action.
+ * @apiError (403) AUTHENTICATION_NEEDED You are not allowed to perform this action.
+ * @apiError (404) GAME_NOT_FOUND There is no game with the id <code>gameId</code>
+ */
 async function putOneHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Games) Games->Put"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Games) Games->Put"));
 
     let filteredDisplayStatus = 0;
     switch(parseInt(req.body.displayStatus)) {
@@ -213,8 +302,23 @@ async function putOneHandler(req, res) {
         }
     });
 }
+
+/**
+ * @api {delete} /games/:gameId Deletes a Game
+ * @apiName DeleteGame
+ * @apiGroup Games
+ * 
+ * @apiHeader {String} x-access-token JWT Token for authentication
+ * @apiParam {Integer} gameId ID of the Game
+ * 
+ * @apiSuccess (201) GAME_DELETED Game was deleted.
+ * @apiError (403) AUTHENTICATION_BANNED Your account was banned. (Reason included in body)
+ * @apiError (403) AUTHENTICATION_WRONG You are not allowed to perform this action.
+ * @apiError (403) AUTHENTICATION_NEEDED You are not allowed to perform this action.
+ * @apiError (404) GAME_NOT_FOUND There is no game with the id <code>gameId</code>
+ */
 async function deleteOneHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Games) Games->Delete"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Games) Games->Delete"));
 
     let game = await getOneGame({ id: parseInt(req.params.gameid) }).catch(() => { return null; });
     if(game === null) {
@@ -236,8 +340,24 @@ async function deleteOneHandler(req, res) {
     });
 }
 
+/**
+ * @api {put} /game/:gameId/cover Uploads a cover to a Game
+ * @apiName UploadGameCover
+ * @apiGroup Games
+ * 
+ * @apiHeader {String} x-access-token JWT Token for authentication
+ * @apiParam {Integer} gameId The ID of the Game
+ * @apiParam {File} cover multipart/form-data of a png or jpg file
+ * 
+ * @apiSuccess (201) GAME_COVER_UPDATED Cover of game was updated.
+ * @apiError (404) GAME_NOT_FOUND There is no game with the id <code>gameId</code>
+ * @apiError (400) GAME_COVER_WRONGFORMAT Your cover is not a png or jpg file.
+ * @apiError (403) AUTHENTICATION_BANNED Your account was banned. (Reason included in body)
+ * @apiError (403) AUTHENTICATION_WRONG You are not allowed to perform this action.
+ * @apiError (403) AUTHENTICATION_NEEDED You are not allowed to perform this action.
+ */
 async function putOneCoverHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Games) Game->PutCover"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Games) Game->PutCover"));
 
     if(req.params.gameid === '' || req.file === undefined) {
         res.status(400).json({name: "MISSING_FIELDS", text: "Required fields: gameId, cover"});
@@ -270,8 +390,23 @@ async function putOneCoverHandler(req, res) {
         }
     });
 }
+
+/**
+ * @api {delete} /game/:gameId/cover Deletes the cover of a Game
+ * @apiName DeleteGameCover
+ * @apiGroup Games
+ * 
+ * @apiHeader {String} x-access-token JWT Token for authentication
+ * @apiParam {Integer} gameId The ID of the Game
+ * 
+ * @apiSuccess (201) GAME_COVER_UPDATED Cover of game was deleted.
+ * @apiError (404) GAME_NOT_FOUND There is no game with the id <code>gameId</code>
+ * @apiError (403) AUTHENTICATION_BANNED Your account was banned. (Reason included in body)
+ * @apiError (403) AUTHENTICATION_WRONG You are not allowed to perform this action.
+ * @apiError (403) AUTHENTICATION_NEEDED You are not allowed to perform this action.
+ */
 async function deleteOneCoverHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Games) Games->DeleteCover"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Games) Games->DeleteCover"));
 
     if(req.params.gameid === '') {
         res.status(400).json({name: "MISSING_FIELDS", text: "Required fields: gameId"});
