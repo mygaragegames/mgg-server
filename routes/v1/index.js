88f6dc523f966e549e19d2ac756295fcff784e31
Laura Heimann <hello@laura.media>
2021-06-05 22:30:48 +0200

    added discovery routes

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.use('/auth', require('./auth'));
 router.use('/oauth', require('./oauth'));
+router.use('/discovery', require('./discovery'));
 router.use('/games', require('./games'));
 router.use('/gameScreenshots', require('./gameScreenshots'));
 router.use('/gameComments', require('./gameComments'));
