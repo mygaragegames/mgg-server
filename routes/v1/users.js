4db1acb1c589f36f64bd9e355da6a6e9d4f69428
Laura Heimann <hello@laura.media>
2021-06-07 17:24:49 +0200

    added api docs for User

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -8,6 +8,8 @@ const { parseAvatar, parseGameCover, isUsernameValid, isCreatorIDValid } = requi
 const { User } = require('../../sequelize');
 const { getAllUsers, getOneUser, createUser, updateUser, setAvatar, deleteAvatar } = require('../../src/users');
 
+let isDev = process.env.NODE_ENV !== 'prod';
+
 let upload = multer({ dest: '/tmp/'});
 
 router.route('/')
@@ -22,8 +24,15 @@ router.route('/:userid/avatar')
     .put(auth.verifyToken, upload.single('avatar'), putUpdateAvatarHandler)
     .delete(auth.verifyToken, deleteUpdateAvatarHandler)
 
+/**
+ * @api {get} /users Get all users
+ * @apiName GetAllUsers
+ * @apiGroup Users
+ * 
+ * @apiSuccess (200) {Array} users Array of users
+ */
 async function getAllHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Users) Users->Get"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Users) Users->Get"));
 
     let users = await getAllUsers();
 
@@ -36,8 +45,32 @@ async function getAllHandler(req, res) {
     
     res.status(200).json(users);
 }
+
+/**
+ * @api {get} /users/:userId Get detailled information from a user
+ * @apiName GetOneUsers
+ * @apiGroup Users
+ * 
+ * @apiHeader {String} x-access-token (Optional) JWT Token for authentication
+ * @apiParam {Integer} userId The ID of the User
+ * 
+ * @apiSuccess (200) {Integer} id ID of the User
+ * @apiSuccess (200) {String} username Username
+ * @apiSuccess (200) {String} avatarFileName URL to the Users avatar
+ * @apiSuccess (200) {String} pronouns Chosen pronouns
+ * @apiSuccess (200) {String} ingameID Ingame-ID
+ * @apiSuccess (200) {String} socialDiscord Discord username of the User
+ * @apiSuccess (200) {String} socialTwitter Twitter username of the User
+ * @apiSuccess (200) {String} socialYouTube YouTube username of the User
+ * @apiSuccess (200) {DateTime} createdAt Join date of the User
+ * @apiSuccess (200) {DateTime} updatedDate Last time the User updated their profile
+ * @apiSuccess (200) {Array} playlists Playlists created by the User (currently called "Play Later")
+ * @apiSuccess (200) {Array} comments Comments posted by the User
+ * @apiSuccess (200) {Array} games Games published by the User
+ * @apiError (404) USER_NOT_FOUND There is no user with the id <code>:userId</code>
+ */
 async function getOneHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Users) Users->GetOne"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Users) Users->GetOne"));
 
     if(req.params.userid === undefined) {
         res.status(400).json({name: "MISSING_FIELDS", text: "Required fields: userid"});
@@ -53,7 +86,7 @@ async function getOneHandler(req, res) {
         searchOptions = { username: req.params.userid };
     }
 
-    let userData = await getOneUser(searchOptions).catch((error) => { return null; });
+    let userData = await getOneUser(searchOptions, req.userId, req.userRoles).catch((error) => { return null; });
     if(userData === null) {
         res.status(404).json({name: "USER_NOT_FOUND", text: `There is no user with the id ${req.params.userid}`});
         return;
@@ -69,13 +102,6 @@ async function getOneHandler(req, res) {
 
     let filteredGames = [];
     userData.games.forEach((game) => {
-        // Only add display status 1 & 2 games when owner or admin/moderator
-        if(game.displayStatus == 1 || game.displayStatus == 2) {
-            if(req.userId == null) return;
-            if(req.userRoles == null) return;
-            if(game.userId !== req.userId && !req.userRoles.includes('moderator', 'admin')) return;
-        }
-
         game.coverFileName = parseGameCover(game.coverFileName);
         game.user.password = undefined;
         game.user.email = undefined;
@@ -88,8 +114,29 @@ async function getOneHandler(req, res) {
 
     res.status(200).json(userData);
 }
+
+/**
+ * @api {post} /users Creates a User
+ * @apiName CreateUser
+ * @apiGroup Users
+ * 
+ * @apiParam {String} username Username for the User
+ * @apiParam {String} password Password for the User
+ * @apiParam {String} email Email for the User
+ * @apiParam {String} pronouns (Optional) Pronouns for the User
+ * @apiParam {String} gameID (Optional) Ingame-ID for the User
+ * @apiParam {String} socialDiscord (Optional) Discord username for the User
+ * @apiParam {String} socialTwitter (Optional) Twitter username for the User
+ * @apiParam {String} socialYouTube (Optional) YouTube username for the User
+ * 
+ * @apiSuccess (201) {Object} User object of created user
+ * @apiError (409) USERNAME_EMAIL_CONFLICT Username or email is already in use.
+ * @apiError (406) USERNAME_INVALID Username is not valid!
+ * @apiError (406) EMAIL_INVALID Email is not valid!
+ * @apiError (406) USER_INGAMEID_WRONGFORMAT The ingame ID has the wrong format (P-000-000-000).
+ */
 async function postOneHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Users) Users->Post"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Users) Users->Post"));
     
     const data = {
         username: req.body.username,
@@ -123,20 +170,42 @@ async function postOneHandler(req, res) {
             case 409:
                 res.status(409).json({name: "USERNAME_EMAIL_CONFLICT", text: "Username or email is already in use!"});
                 return;
-            case 418:
-                res.status(418).json({name: "USERNAME_INVALID", text: "Username is not valid!"});
+            case 406:
+                res.status(406).json({name: "USERNAME_INVALID", text: "Username is not valid!"});
                 return;
             case 406:
                 res.status(406).json({name: "EMAIL_INVALID", text: "Email is not valid!"});
                 return;
-            case 400:
-                res.status(400).json({name: "USER_INGAMEID_WRONGFORMAT", text: "The ingame ID has the wrong format (P-000-000-000)."});
+            case 406:
+                res.status(406).json({name: "USER_INGAMEID_WRONGFORMAT", text: "The ingame ID has the wrong format (P-000-000-000)."});
                 return;
         }
     });
 }
+
+/**
+ * @api {put} /users/:userId Updates a User
+ * @apiName UpdateUser
+ * @apiGroup Users
+ * 
+ * @apiHeader {String} x-access-token JWT Token for authentication
+ * @apiParam {Integer} userId The ID of the User
+ * 
+ * @apiParam {String} pronouns (Optional) Pronouns for the User
+ * @apiParam {String} gameID (Optional) Ingame-ID for the User
+ * @apiParam {String} socialDiscord (Optional) Discord username for the User
+ * @apiParam {String} socialTwitter (Optional) Twitter username for the User
+ * @apiParam {String} socialYouTube (Optional) YouTube username for the User
+ * 
+ * @apiSuccess (201) USER_UPDATED User was updated.
+ * @apiError (404) USER_NOT_FOUND There is no user with the id <code>userId</code>
+ * @apiError (406) USER_INGAMEID_WRONGFORMAT The ingame ID has the wrong format (P-000-000-000).
+ * @apiError (403) AUTHENTICATION_BANNED Your account was banned. (Reason included in body)
+ * @apiError (403) AUTHENTICATION_WRONG You are not allowed to perform this action.
+ * @apiError (403) AUTHENTICATION_NEEDED You are not allowed to perform this action.
+ */
 async function putOneHandler(req, res) {
-    console.log(chalk.grey("[mgg-server] (Users) Users->Put"));
+    if(isDev) console.log(chalk.grey("[mgg-server] (Users) Users->Put"));
 
     const data = {
         pronouns: req.body.pronouns,
@@ -166,12 +235,30 @@ async function putOneHandler(req, res) {
             default:
                 res.status(500).json({name: "UNKNOWN_ERROR", text: "User could not be updated."});
                 return;
-            case 400:
-                res.status(400).json({name: "USER_INGAMEID_WRONGFORMAT", text: "The ingame ID has the wrong format (P-000-000-000)."});
+            case 406:
+                res.status(406).json({name: "USER_INGAMEID_WRONGFORMAT", text: "The ingame ID has the wrong format (P-000-000-000)."});
                 return;
         }
     });
 }
+
+/**
+ * @api {put} /users/:userId/avatar Updates a Users avatar
+ * @apiName UpdateUserAvatar
+ * @apiGroup Users
+ * 
+ * @apiHeader {String} x-access-token JWT Token for authentication
+ * @apiParam {Integer} userId The ID of the User
+ * 
+ * @apiParam {File} avatar multipart/form-data of a png or jpg file
+ * 
+ * @apiSuccess (201) USER_AVATAR_UPDATED Avatar was updated.
+ * @apiError (404) USER_NOT_FOUND There is no user with the id <code>userId</code>
+ * @apiError (400) USER_AVATAR_WRONGFORMAT Your avatar is not a png or jpg file.
+ * @apiError (403) AUTHENTICATION_BANNED Your account was banned. (Reason included in body)
+ * @apiError (403) AUTHENTICATION_WRONG You are not allowed to perform this action.
+ * @apiError (403) AUTHENTICATION_NEEDED You are not allowed to perform this action.
+ */
 async function putUpdateAvatarHandler(req, res) {
     console.log(chalk.grey("[mgg-server] (Users) Users->PutUpdateAvatar"));
 
@@ -211,6 +298,21 @@ async function putUpdateAvatarHandler(req, res) {
         }
     });
 }
+
+/**
+ * @api {delete} /user/:userId/avatar Deletes the avatar of a User
+ * @apiName DeleteUserAvatar
+ * @apiGroup Users
+ * 
+ * @apiHeader {String} x-access-token JWT Token for authentication
+ * @apiParam {Integer} userId The ID of the User
+ * 
+ * @apiSuccess (201) USER_AVATAR_UPDATED Avatar was deleted.
+ * @apiError (404) USER_NOT_FOUND There is no user with the id <code>userId</code>
+ * @apiError (403) AUTHENTICATION_BANNED Your account was banned. (Reason included in body)
+ * @apiError (403) AUTHENTICATION_WRONG You are not allowed to perform this action.
+ * @apiError (403) AUTHENTICATION_NEEDED You are not allowed to perform this action.
+ */
 async function deleteUpdateAvatarHandler(req, res) {
     console.log(chalk.grey("[mgg-server] (Users) Users->DeleteUpdateAvatar"));
 
@@ -233,7 +335,7 @@ async function deleteUpdateAvatarHandler(req, res) {
     }
 
     deleteAvatar( user ).then(() => {
-        res.status(200).json({name: "USER_AVATAR_DELETED", text: "Avatar was deleted."});
+        res.status(200).json({name: "USER_AVATAR_UPDATED", text: "Avatar was deleted."});
         return;
     }).catch((error) => {
         if(error === 404) {
