b15e4491bf04c509c32c50970ff62f1ae21534de
Laura Heimann <hello@laura.media>
2021-06-08 17:30:46 +0200

    fixed getOneUser

--- src/users.js
+++ src/users.js
@@ -41,14 +41,14 @@ function getOneUser( searchOptions, userId = 0, userRoles = []) {
                 {
                     model: Game, as: "games",
                     include: { model: User, as: "user" },
-                    where: [
+                    where: Sequelize.where(
                         Sequelize.literal(`1 = CASE
                             WHEN ${overrideDisplayStatus} = true THEN 1
                             WHEN games.displayStatus = 2 AND games.userId = ${userId} THEN 1
                             WHEN games.displayStatus = 0 THEN 1
                             ELSE 2
                         END`)
-                    ],
+                    ),
                     order: [
                         ['games.createdAt', 'DESC']
                     ],
