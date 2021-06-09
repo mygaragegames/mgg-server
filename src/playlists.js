38fd1809ab31a48402c79b0928dba6d06a12b789
Laura Heimann <hello@laura.media>
2021-06-09 10:52:53 +0200

    removed display status honor in playlists for now

--- src/playlists.js
+++ src/playlists.js
@@ -4,30 +4,20 @@ const { User, Game, Playlist } = require('../sequelize');
 function getOnePlaylist( searchOptions, userId = 0, userRoles = []) {
     let overrideDisplayStatus = userRoles.includes('moderator', 'admin');
 
+    // TODO: Fix honoring displayStatus
+
     return new Promise((resolve, reject) => {
         Playlist.findOne({
                 where: searchOptions,
                 include: [
                     {
                         model: User,
-                        as: 'user'
+                        as: 'user',
                     },
                     {
                         model: Game,
-                        as: 'games',
-                        required: false,
-                        include: { model: User, as: 'user' },
-                        where: {
-                            [Sequelize.Op.and]: [
-                                Sequelize.literal(`1 = CASE
-                                    WHEN ${overrideDisplayStatus} = true THEN 1
-                                    WHEN games.displayStatus = 2 AND games.userId = ${userId} THEN 1
-                                    WHEN games.displayStatus = 1 THEN 1
-                                    WHEN games.displayStatus = 0 THEN 1
-                                    ELSE 2
-                                END`)
-                            ]
-                        },
+                        as: "games",
+                        include: { model: User, as: "user" },
                         order: [
                             ['games.createdAt', 'DESC']
                         ],
